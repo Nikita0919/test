a33a2a85b3094dbbaacba9ea870baf5350ada176
Nikita0919 <zoginnikita29@gmail.com>
2024-07-01 19:00:28 +0300

    change function print

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<fstream>
+
+
+using namespace std;
+
+void summ(int a,int b);
+
+int main()
+{
+
+}
+void summ(int a, int b)
+{
+    cout << a + b << endl;
+}
